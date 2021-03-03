ec7df2ae024e7fc3bb76b2162b1a2a073e8a7d82
rot <appl.rot13@gmail.com>
2021-03-04 01:12:59 +0900

    Create new

---
+++ ListHashes.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set input_dir=%~dp0
+set output_file=HashList.txt
+set algorithm=SHA1
+
+for /f "tokens=*" %%f in (
+  'dir %input_dir% /a-d /b /s'
+) do for /f %%h in (
+  'certutil -hashfile "%%f" %algorithm% ^| findstr /v ":"'
+) do echo %%h %%f>>%output_file%
